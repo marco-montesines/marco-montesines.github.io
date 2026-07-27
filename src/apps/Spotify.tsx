ae4f96d4a5ac2c64be6450cf9a6106a01d54afd3
Marco Montesines <mamontesines@hotmail.com>
2026-07-27 14:26:06 +0200

    update spotify playlist

--- src/apps/Spotify.tsx
+++ src/apps/Spotify.tsx
@@ -7,7 +7,7 @@ export function Spotify() {
   return (
     <iframe
       className="app-frame"
-      src="https://open.spotify.com/embed/playlist/37i9dQZEVXbMDoHDwVN2tF"
+      src="https://open.spotify.com/embed/playlist/37i9dQZEVXbNBz9cRCSFkY"
       title="Spotify"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
